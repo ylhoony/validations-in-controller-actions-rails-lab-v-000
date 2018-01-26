0c908331b2ce6e3d5b7bc454feb383fb201cfb7a
Hoon Lee <yl.hoony@gmail.com>
2018-01-26 19:12:50 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
-
+  validates :content, length: { minimum: 100 }
 
 end
